c4fa2111078eb1f603a50c6f8e2927ae05a8bf38
Franco Montenegro <fmontenegro2207@gmail.com>
2023-09-02 22:07:00 -0500

    se termina design de journal y entries

--- src/journal/layout/JournalLayout.jsx
+++ src/journal/layout/JournalLayout.jsx
@@ -1,16 +1,21 @@
-import { Box } from "@mui/material"
+import { Box, Toolbar } from "@mui/material"
+import { NavBar, SideBar } from "../components";
 
-const drawerWidth = 240;
+const drawerWidth = 280;
 
 const JournalLayout = ({ children }) => {
   return (
     <Box sx={{ display: 'flex' }}>
         {/* Navbar drawerWidth */}
 
+        <NavBar drawerWidth={drawerWidth} />
+
         {/* Sidebar drawerWidth */}
 
+        <SideBar drawerWidth={drawerWidth} />
+
         <Box component='main' sx={{ flexGrow: 1, p: 3 }}>
-            {/* Toolbar */}
+            <Toolbar />
             
             {children}
         </Box>
